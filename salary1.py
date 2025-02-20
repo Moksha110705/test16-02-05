b0e954dd44e6cd0ed82f58951ba0882aa6421241
Venu Gopal M <moksha.v@s.amity.edu>
2025-02-20 20:37:29 +0530

    new python file

---
+++ salary1.py
@@ -0,0 +1,10 @@
+workingdays= int (input("Enter number of days the employee has worked in a month "))
+if (workingdays >= 15):
+    print ("Your salary will get credited soon ")
+    if ( workingdays >=20):
+        print ("You are granted with a casual leave next month ")
+    else:
+        print(" No leave for the month!! Work harder! ")
+else:
+    print ("Your salary will not be credited n u wont get a leave  ")
+
